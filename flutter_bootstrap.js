0376c46e580677d8c0a8fa24c278444b28e71b44
exmKrd <derick.maxime.pro@gmail.com>
2024-07-06 23:42:52 +0200

    maj

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3211628466"
+    serviceWorkerVersion: "3757657723"
   }
 });
